ffee42f47530917935ed98aaa88d2c2cb964e9c4
Doggy-068 <728724654@qq.com>
2023-06-27 22:29:21 +0800

    feat: upgrade drift

--- lib/database/platform/web.dart
+++ lib/database/platform/web.dart
@@ -1,10 +1,13 @@
 import 'package:drift/drift.dart';
-import 'package:drift/web.dart';
+import 'package:drift/wasm.dart';
 
 LazyDatabase openConnection() {
   return LazyDatabase(() async {
-    return WebDatabase.withStorage(
-      await DriftWebStorage.indexedDbIfSupported('todo_app'),
+    final result = await WasmDatabase.open(
+      databaseName: 'todo_app',
+      sqlite3Uri: Uri.parse('/sqlite3.wasm'),
+      driftWorkerUri: Uri.parse('/drift_worker.js'),
     );
+    return result.resolvedExecutor;
   });
 }
