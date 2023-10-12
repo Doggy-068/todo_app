4dfd3e7c1295cb1e77680d867d7fd80b35daf042
Doggy-068 <728724654@qq.com>
2023-10-12 21:19:58 +0800

    chore: reset

--- lib/database/platform/unsupported.dart
+++
@@ -1,3 +0,0 @@
-import 'package:drift/drift.dart';
-
-LazyDatabase openConnection() => throw UnimplementedError();
--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_app/screen/home/index.dart';
-import 'package:todo_app/database/index.dart';
-import 'package:todo_app/storage/index.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
-  runApp(Provider<AppDatabase>(
-    create: (context) => AppDatabase(),
-    child: const App(),
-    dispose: (context, db) => db.close(),
-  ));
+  runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
   @override
-  State<App> createState() => AppState();
-}
-
-class AppState extends State<App> {
-  Locale _locale = const Locale('en');
-  Color _colorSchemeSeed = Colors.blue;
-
-  void setLocale(Locale locale) {
-    setState(() {
-      _locale = locale;
-    });
-  }
-
-  void setColor(Color color) {
-    setState(() {
-      _colorSchemeSeed = color;
-    });
-  }
-
-  @override
-  void initState() {
-    Storage.getThemeColor().then((value) => setColor(value));
-    Storage.getLocale().then((value) => setLocale(value));
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Provider.value(
-      value: this,
-      child: MaterialApp(
-        locale: _locale,
-        supportedLocales: const [
-          Locale('en'),
-          Locale('zh'),
-        ],
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        theme: ThemeData(
-          useMaterial3: true,
-          colorSchemeSeed: _colorSchemeSeed,
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello'),
         ),
-        home: const ScreenHome(),
       ),
     );
   }
